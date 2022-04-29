cae3a6c3e498e53750c55bef3ee0218d2b27cdfd
SaiKrishnaPrasadK <saikskp@gmail.com>
2022-04-29 10:59:31 -0500

    whiteboard and classroom

--- Assets/Scripts/Whiteboard.cs
+++ Assets/Scripts/Whiteboard.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Whiteboard : MonoBehaviour
 {
+
+    public Texture2D texture;
+
+    public Vector2 textureSize = new Vector2(x: 2048, y: 2048);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var r = GetComponent<Renderer>();
+        texture = new Texture2D(width: (int)textureSize.x, height: (int)textureSize.y);
+        r.material.mainTexture = texture;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
